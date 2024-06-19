05948b6dcdfd7b6700e9701232c91bd38549a1c6
rakshithsingh5 <rakshithsinghthakur@gmail.com>
2024-06-19 11:17:52 -0500

    Update HW1_RAKSHITH.sql

--- HW1_RAKSHITH.sql
+++ HW1_RAKSHITH.sql
@@ -1,9 +1,9 @@
 --1Q
-SELECT ProductID,Name,Color, ListPrice
+SELECT ProductID, [Name], Color, ListPrice
 FROM Production.Product
 
 --2Q
-SELECT ProductID, Name, Color, ListPrice
+SELECT ProductID, [Name], Color, ListPrice
 FROM Production.Product
 WHERE ListPrice != 0
 
@@ -13,57 +13,57 @@ FROM Production.Product
 WHERE Color IS NULL
 
 --4Q
-SELECT ProductID, Name, Color, ListPrice
+SELECT ProductID, [Name], Color, ListPrice
 FROM Production.Product
 WHERE Color IS NOT NULL
 
 --5Q
-SELECT ProductID, Name, Color, ListPrice
+SELECT ProductID, [Name], Color, ListPrice
 FROM Production.Product
 WHERE Color IS NOT NULL AND ListPrice > 0
 
 --6Q
-SELECT [Name] +'-'+  Color AS [Name and color]
+SELECT [Name] +' & '+  Color AS [Name and color]
 FROM Production.Product
 WHERE Color IS NOT NULL
 
 --7Q
-SELECT TOP 6 'NAME '+ [NAME] +' -- '+ 'COLOR: '+ Color 
+SELECT TOP 6 'NAME: '+ [NAME] +' -- '+ 'COLOR: '+ Color AS [Name and color]
 FROM Production.Product
 WHERE Color IS NOT NULL
 
 --8Q
-SELECT ProductID, Name
+SELECT ProductID, [Name]
 FROM Production.Product
 WHERE ProductID BETWEEN 400 and 500
 
 --9Q
-SELECT ProductID, Name, Color
+SELECT ProductID, [Name], Color
 FROM Production.Product
-WHERE Color NOT IN('black', 'blue')
+WHERE Color IN ('black', 'blue')
 
 --10Q
-SELECT Name, ListPrice
+SELECT [Name]
 FROM Production.Product
-WHERE Name LIKE 'S%'
+WHERE [Name] LIKE 'S%'
 
 --11Q
-SELECT TOP 6 Name, ListPrice
+SELECT TOP 6 [Name], ListPrice
 FROM Production.Product
-WHERE NAME LIKE 'S%'
-ORDER BY Name
+WHERE [NAME] LIKE 'S%'
+ORDER BY [Name]
 
 --12Q
-SELECT TOP 5 Name, ListPrice
+SELECT TOP 5 [Name], ListPrice
 FROM Production.Product
-WHERE Name LIKE 'S%' OR Name LIKE 'A%'
-ORDER BY Name
+WHERE [Name] LIKE 'S%' OR Name LIKE 'A%'
+ORDER BY [Name]
 
 --13Q
-SELECT Name, ListPrice
+SELECT [Name], ListPrice
 FROM Production.Product
-WHERE Name LIKE 'SPO[^K]%'
-ORDER BY Name
+WHERE [Name] LIKE 'SPO[^K]%'
+ORDER BY [Name]
 
 --14Q
 SELECT DISTINCT Color
@@ -75,3 +75,4 @@ ORDER BY Color DESC
 SELECT DISTINCT ProductSubcategoryID, Color
 FROM Production.Product
 WHERE Color IS NOT NULL AND ProductSubcategoryID IS NOT NULL
+ORDER BY ProductSubcategoryID, Color
